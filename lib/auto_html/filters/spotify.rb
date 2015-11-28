2fe3227daf45c01754373ff00fe7b6dc1ecb5cca
Akshat Agrawal <akshat.ag77@gmail.com>
2015-11-28 00:25:49 -0500

    spotify retrying

--- lib/auto_html/filters/spotify.rb
+++ lib/auto_html/filters/spotify.rb
@@ -2,24 +2,11 @@ AutoHtml.add_filter(:spotify).with(:width => '100%', :height => 166, :class => "
   require 'uri'
   require 'net/http'
 
-  if text.match(/(https?:\/\/)?(www\.)?open\.spotify\.com\/track\/\S*/)
-    uri = text.split('/')[-1]
+  text.gsub(/(https?:\/\/)?(www\.)?open\.spotify\.com\/track\/(\S*)?/) do
+    new_uri = $3
     width = options[:width]
     height = options[:height]
-    color = options[:color]
     classname = options[:class]
-    %{<div class="#{classname}" style="width: #{width}px; height: #{height}px;"><iframe width="#{width}" height="#{height}" scrolling="no" frameborder="no" src="https://embed.spotify.com/?uri=spotify:track:#{uri}"></iframe></div>}
-  else
+    %{<div class="#{classname}" style="width: #{width}px; height: #{height}px;"><iframe width="#{width}" height="#{height}" scrolling="no" frameborder="no" src="https://embed.spotify.com/?uri=spotify:track:#{new_uri}"></iframe></div>}
   end
-
-  # text.gsub(/(https?:\/\/)?(www.)?open.spotify\.com\/track\/\S*/) do |match|
-  #   new_uri = match.to_s
-  #   new_uri = (new_uri =~ /^https?\:\/\/.*/) ? URI(new_uri) : URI("http://#{new_uri}")
-  #   new_uri.normalize!
-  #   width = options[:width]
-  #   height = options[:height]
-  #   color = options[:color]
-  #   classname = options[:class]
-  #   %{<div class="#{classname}" style="width: #{width}px; height: #{height}px;"><iframe width="#{width}" height="#{height}" scrolling="no" frameborder="no" src="https://embed.spotify.com/?uri=spotify:track:#{new_uri}"></iframe></div>}
-  # end
 end
